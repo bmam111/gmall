12db0b5484fdb3d4e3ad2c22cd0fa4d8f7ec6350
mason <mason@126.com>
2019-06-28 21:10:58 +0800

    拆解出parent和api

---
+++ gmall-api/src/main/java/com/atguigu/gmall/bean/UserInfo.java
@@ -0,0 +1,115 @@
+package com.atguigu.gmall.bean;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+public class UserInfo {
+
+    //编号
+    @Id
+    private  String id;
+
+    //用户名称
+    @Column
+    private  String loginName;
+
+    //用户昵称
+    @Column
+    private  String nickName;
+
+    //用户密码
+    @Column
+    private  String passwd;
+
+    //用户姓名
+    @Column
+    private  String name;
+
+    //手机号
+    @Column
+    private  String phoneNum;
+
+    //邮箱
+    @Column
+    private  String email;
+
+    //头像
+    @Column
+    private  String headImg;
+
+    //用户级别
+    @Column
+    private  String userLevel;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHeadImg() {
+        return headImg;
+    }
+
+    public void setHeadImg(String headImg) {
+        this.headImg = headImg;
+    }
+
+    public String getUserLevel() {
+        return userLevel;
+    }
+
+    public void setUserLevel(String userLevel) {
+        this.userLevel = userLevel;
+    }
+}
